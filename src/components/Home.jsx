dca62f9316a5a292bc09e92ad92510dd7f006cd1
Vaishali Singh <vaishali231305@gmail.com>
2024-02-18 23:46:20 +0530

    some style change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,13 +53,67 @@
 // export default Navigation;
 
 
-import React from 'react';
+// import React from 'react';
 
+// const Navigation = () => {
+//     return (
+//         <div className="navigation_container bg-zinc-800 rounded-full text-zinc-400 h-16 md:h-16 lg:h-18 xl:h-20 flex justify-center items-center fixed top-10 left-1/2 transform -translate-x-1/2 overflow-hidden">
+//             <nav id="navigation" className="navigation scroll-down">
+//                 <ul className="navigation_link-wrapper flex space-x-4">
+//                     <li aria-hidden="true" className="navigation_mobile-spacer-wrapper">
+//                         <div className="navigation_mobile-spacer" style={{ width: '0px' }}></div>
+//                     </li>
+//                     <li id="navigation_item-0" data-navigation-id="introduction" className="navigation_link navigation_selected">
+//                         <a href="#introduction" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>The Gemini era</span>
+//                         </a>
+//                     </li>
+//                     <li id="navigation_item-1" data-navigation-id="gemini-1.5" className="navigation_link">
+//                         <a href="#gemini-1.5" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>Gemini 1.5</span>
+//                         </a>
+//                     </li>
+//                     <li id="navigation_item-2" data-navigation-id="gemini-1.0" className="navigation_link">
+//                         <a href="#gemini-1.0" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>Gemini 1.0</span>
+//                         </a>
+//                     </li>
+//                     <li id="navigation_item-3" data-navigation-id="safety" className="navigation_link">
+//                         <a href="#safety" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>Safety</span>
+//                         </a>
+//                     </li>
+//                     <li id="navigation_item-4" data-navigation-id="gemini-apps" className="navigation_link">
+//                         <a href="#gemini-apps" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>Gemini Apps</span>
+//                         </a>
+//                     </li>
+//                     <li id="navigation_item-5" data-navigation-id="build-with-gemini" className="navigation_link">
+//                         <a href="#build-with-gemini" className="hover:text-white hover:border-b-2 border-transparent">
+//                             <span>Build with Gemini</span>
+//                         </a>
+//                     </li>
+//                     <li aria-hidden="true" className="navigation_mobile-spacer-wrapper">
+//                         <div className="navigation_mobile-spacer" style={{ width: '0px' }}>&nbsp;</div>
+//                     </li>
+//                 </ul>
+//                 <div className="indicator_wrapper">
+//                     <div className="mobile_indicator" style={{ translate: 'none', rotate: 'none', scale: 'none', transform: 'translate(27px, 0px) scale(0.1362, 1)' }}></div>
+//                 </div>
+//             </nav>
+//         </div>
+//     );
+// };
+
+// export default Navigation;
+
+import React from 'react';
+import './style.css';
 const Navigation = () => {
     return (
         <div className="navigation_container bg-zinc-800 rounded-full text-zinc-400 h-16 md:h-16 lg:h-18 xl:h-20 flex justify-center items-center fixed top-10 left-1/2 transform -translate-x-1/2 overflow-hidden">
             <nav id="navigation" className="navigation scroll-down">
-                <ul className="navigation_link-wrapper flex space-x-5">
+                <ul className="navigation_link-wrapper flex space-x-4">
                     <li aria-hidden="true" className="navigation_mobile-spacer-wrapper">
                         <div className="navigation_mobile-spacer" style={{ width: '0px' }}></div>
                     </li>
